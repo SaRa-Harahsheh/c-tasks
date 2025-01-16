67a605c83a9a893c2b7f7fdc78d63042964ffbd0
SaRa-Harahsheh <saraharahsheh0@gmail.com>
2025-01-16 16:17:59 +0300

    Add project files.

---
+++ c# tasks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = Console.ReadLine("sara");
+        }
+    }
+}
