1ed9cb40413ba2c8eed2612adafcf932b5fa92f4
SaRa-Harahsheh <saraharahsheh0@gmail.com>
2025-01-20 14:04:51 +0300

    task2-c#

--- task2-19-1/Program.cs
+++ task2-19-1/Program.cs
@@ -10,8 +10,17 @@ namespace task2_19_1
 {
     internal class Program
     {
+
+        static double miles (double kilometer)
+        {
+
+            return kilometer * 0.621371;
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Q6. ");
+            Console.WriteLine(miles(15)+"m/h");
 
             //1--------
             Console.WriteLine("Q1.plz insert double num ");
@@ -26,32 +35,44 @@ namespace task2_19_1
             //2-------------
             Console.WriteLine("Q2.plz insert number");
 
-            string age = Console.ReadLine();
-
-            string agee = age;
-            Console.WriteLine($"Your number is: {agee}");
+            int number = Convert.ToInt32(Console.ReadLine());
+             string age = Convert.ToString(number); 
+            Console.WriteLine("Your number is:"+ age);
 
 
             //3-------------
             Console.WriteLine("Q3.plz insert text");
 
             string text = Console.ReadLine();
-            Console.WriteLine($"UpperCase: " +text.ToUpper());   
-            Console.WriteLine($"lowerCase: " + text.ToLower());
+            Console.WriteLine("UpperCase: " +text.ToUpper());   
+            Console.WriteLine("lowerCase: " + text.ToLower());
+
+
 
 
             //4---------------
             Console.WriteLine("Q3.insert your first name & last name");
-            string name = Console.ReadLine();
-            string fName = name.Substring(0, 5);
-            string lName = name.Substring(6);
+            string name = "sara Harahsheh";
+            string fName = name.Substring(0, 4);
+            string lName = name.Substring(5);
             Console.WriteLine(fName);
             Console.WriteLine(lName);
             Console.WriteLine(fName.Length + lName.Length);
             Console.WriteLine();
 
+            //Console.WriteLine("Q3.insert your first name & last name");
+            //string name = "sara Harahsheh";
+            //string fName = name.Substring(0, name.IndexOf(" "));
+            //string lName = name.Substring(name.IndexOf(" ")+1);
+            //Console.WriteLine(fName);
+            //Console.WriteLine(lName);
+            //Console.WriteLine(fName.Length + lName.Length);
+            //Console.WriteLine();
+
+
+
             //5---------------------
-            
+
             Console.WriteLine("Enter Two Numbers:");
             string x = Console.ReadLine();
             string y = Console.ReadLine();
@@ -65,7 +86,14 @@ namespace task2_19_1
                 Console.WriteLine($"{num2} is larger than {num1}");
             else
                 Console.WriteLine("Numbers are equal");
-            Console.WriteLine();
+           
+
+
+
+            
+
+
+
         }
     }
 }
